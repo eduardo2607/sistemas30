607803faa1c50f49cfd1b05344f5feee025b8534
saikoner234 <93045481+saikoner234@users.noreply.github.com>
2022-01-07 00:27:39 -0600

    caso

--- caso practico/casopract.cpp
+++ caso practico/casopract.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <stdio.h>
-
+#include "area.h"
 #include "volumen.h"
 
 using namespace std;
 
+
 int main() {
-	int c, d, e, f;
-
-
+	int c, d, e, f, g, h;
+  
+  printf("el siguiente programita nos permite obtener el area y volumen de distintas figuras geometricas ");
+  
   printf("Dame la arista del cubo: ");
   cin>>c;
 	 
@@ -19,12 +21,17 @@ int main() {
   
   printf("Dame el radio de la esfera: ");
   cin>>f;
-    
   
+  printf("Dame la base del triangulo: ");
+  cin>>g;
+      
+  printf("Dame la altura del triangulo: ");
+  cin>>h; 
 
   cout<<"el volumen de un cubo en esta medida es = "<<volumenCubo(c)<<endl;
   cout<<"el volumen del cono es = "<<volumenCono(d, e)<<endl;
   cout<<"el volumen de la esfera = "<<volumenEsfera(f)<<endl;
+  cout<<"el el area del triangulo es = "<<area(g, h)<<endl;
 
   return 0;
 }
